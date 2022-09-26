938c7ac76b5af55ea1474d62f3ddf4ca1e30832d
DIBIN18 <104697691+DIBIN18@users.noreply.github.com>
2022-09-26 21:40:05 +0800

    Revert "jovs"

--- Admin Login/PayrollReport.Designer.cs
+++ Admin Login/PayrollReport.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.panel3 = new System.Windows.Forms.Panel();
-            this.dgvDailyPayrollReport = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.cb_SortBy = new System.Windows.Forms.ComboBox();
             this.lbl_SortBy = new System.Windows.Forms.Label();
@@ -46,37 +49,74 @@
             this.label9 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDailyPayrollReport)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel3.Controls.Add(this.dgvDailyPayrollReport);
+            this.panel3.Controls.Add(this.dataGridView1);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label4);
             this.panel3.Location = new System.Drawing.Point(16, 152);
-            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(827, 426);
             this.panel3.TabIndex = 30;
             // 
-            // dgvDailyPayrollReport
+            // label3
             // 
-            this.dgvDailyPayrollReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            this.dgvDailyPayrollReport.Location = new System.Drawing.Point(3, 3);
-            this.dgvDailyPayrollReport.Name = "dgvDailyPayrollReport";
-            this.dgvDailyPayrollReport.Size = new System.Drawing.Size(819, 418);
-            this.dgvDailyPayrollReport.TabIndex = 5;
-            this.dgvDailyPayrollReport.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDailyPayrollReport_CellClick);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(640, 17);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Net Pay";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(472, 17);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 24);
+            this.label8.TabIndex = 3;
+            this.label8.Text = "Deductions";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(295, 17);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(104, 24);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Gross Pay";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 17);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 24);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Employee ID";
             // 
             // panel2
             // 
@@ -84,7 +124,7 @@
             this.panel2.Controls.Add(this.cb_SortBy);
             this.panel2.Controls.Add(this.lbl_SortBy);
             this.panel2.Location = new System.Drawing.Point(624, 99);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(219, 37);
             this.panel2.TabIndex = 29;
@@ -105,7 +145,7 @@
             "Deductions",
             "Net Pay"});
             this.cb_SortBy.Location = new System.Drawing.Point(84, 3);
-            this.cb_SortBy.Margin = new System.Windows.Forms.Padding(2);
+            this.cb_SortBy.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.cb_SortBy.Name = "cb_SortBy";
             this.cb_SortBy.Size = new System.Drawing.Size(132, 30);
             this.cb_SortBy.TabIndex = 10;
@@ -132,7 +172,7 @@
             this.panel1.Controls.Add(this.pictureBox5);
             this.panel1.Controls.Add(this.tb_Search);
             this.panel1.Location = new System.Drawing.Point(308, 99);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(305, 37);
             this.panel1.TabIndex = 28;
@@ -142,7 +182,7 @@
             this.pictureBox5.BackColor = System.Drawing.Color.White;
             this.pictureBox5.Image = global::Admin_Login.Properties.Resources.Search_Icon;
             this.pictureBox5.Location = new System.Drawing.Point(7, 4);
-            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox5.Name = "pictureBox5";
             this.pictureBox5.Size = new System.Drawing.Size(19, 28);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -156,7 +196,7 @@
             this.tb_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.tb_Search.ForeColor = System.Drawing.SystemColors.InactiveCaption;
             this.tb_Search.Location = new System.Drawing.Point(36, 6);
-            this.tb_Search.Margin = new System.Windows.Forms.Padding(2);
+            this.tb_Search.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tb_Search.Name = "tb_Search";
             this.tb_Search.Size = new System.Drawing.Size(259, 21);
             this.tb_Search.TabIndex = 6;
@@ -181,7 +221,7 @@
             this.dtp_Date.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.dtp_Date.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
             this.dtp_Date.Location = new System.Drawing.Point(98, 19);
-            this.dtp_Date.Margin = new System.Windows.Forms.Padding(2);
+            this.dtp_Date.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtp_Date.Name = "dtp_Date";
             this.dtp_Date.Size = new System.Drawing.Size(224, 28);
             this.dtp_Date.TabIndex = 31;
@@ -203,7 +243,7 @@
             this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel4.Controls.Add(this.lbl_Period);
             this.panel4.Location = new System.Drawing.Point(350, 19);
-            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel4.Name = "panel4";
             this.panel4.Size = new System.Drawing.Size(223, 30);
             this.panel4.TabIndex = 33;
@@ -264,7 +304,7 @@
             this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox2.Image = global::Admin_Login.Properties.Resources.HolidaySettings_Icon;
             this.pictureBox2.Location = new System.Drawing.Point(265, 101);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(28, 32);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -278,7 +318,7 @@
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pictureBox1.Image = global::Admin_Login.Properties.Resources.RoundedRectangle_Shape;
             this.pictureBox1.Location = new System.Drawing.Point(98, 99);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(206, 37);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -287,35 +327,13 @@
             this.pictureBox1.Tag = "btn_HolidaySettings";
             this.pictureBox1.Click += new System.EventHandler(this.Btn_HolidaySettings_Click);
             // 
-            // label3
+            // dataGridView1
             // 
-            this.label3.AutoSize = true;
-            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(99)))), ((int)(((byte)(110)))), ((int)(((byte)(114)))));
-            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(762, 25);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 24);
-            this.label3.TabIndex = 40;
-            this.label3.Tag = "";
-            this.label3.Text = "Export";
-            this.label3.Click += new System.EventHandler(this.btnExportPayroll);
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pictureBox3.Image = global::Admin_Login.Properties.Resources.RoundedRectangle_Shape;
-            this.pictureBox3.Location = new System.Drawing.Point(750, 18);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(91, 37);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox3.TabIndex = 39;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Tag = "btn_HolidaySettings";
-            this.pictureBox3.Click += new System.EventHandler(this.btnExportPayroll);
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(186, 70);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(477, 317);
+            this.dataGridView1.TabIndex = 5;
             // 
             // PayrollReport
             // 
@@ -324,8 +342,6 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(854, 592);
             this.ControlBox = false;
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.pictureBox1);
@@ -339,14 +355,14 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dtp_Date);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "PayrollReport";
             this.Tag = "btn_HolidaySettings";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.PayrollReport_Load);
             this.Click += new System.EventHandler(this.Btn_HolidaySettings_Click);
             this.panel3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgvDailyPayrollReport)).EndInit();
+            this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
             this.panel1.ResumeLayout(false);
@@ -356,7 +372,7 @@
             this.panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -365,6 +381,10 @@
         #endregion
 
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.ComboBox cb_SortBy;
         private System.Windows.Forms.Label lbl_SortBy;
@@ -381,8 +401,6 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.DataGridView dgvDailyPayrollReport;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Admin Login/PayrollReport.cs
+++ Admin Login/PayrollReport.cs
@@ -8,9 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using Syncfusion.XlsIO;
-using System.IO;
-
 namespace Admin_Login
 {
     public partial class PayrollReport : Form
@@ -36,11 +33,11 @@ namespace Admin_Login
                 lbl_Period.Text = Reader.GetValue(1).ToString() + dtp_Date.Value.ToString(", yyyy");
                 lbl_Holiday.Text = Reader.GetValue(0).ToString() + "|" + Reader.GetValue(2).ToString();
             }
-            SqlCommand cmd = new SqlCommand("Select * from Computation",connection);
+            SqlCommand cmd = new SqlCommand("Select EmployeeID,Payroll_ID,BasicRate,TotalRecordHours,TotalOvertimeHours FROM Payroll",connection);
             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             adapter.Fill(dt);
-            dgvDailyPayrollReport.DataSource = dt;
+            dataGridView1.DataSource = dt;
 
 
         }
@@ -104,69 +101,6 @@ namespace Admin_Login
             Menu menu = (Menu)Application.OpenForms["Menu"];
             menu.Text = "Fiona's Farm and Resort - Holiday Settings";
             menu.Menu_Load(menu, EventArgs.Empty);
-        }
-        int i = 1;
-        private void btnExportPayroll(object sender, EventArgs e)
-        {
-            tagadelete();
-            tagaInsertPayroll();
-            SqlConnection connection = new SqlConnection(login.connectionString);
-            connection.Open();
-            string filldt = "select * from Payroll ";
-            SqlCommand command2 = new SqlCommand(filldt, connection);
-            SqlDataAdapter adapter = new SqlDataAdapter(command2);
-            DataTable dt = new DataTable();
-            adapter.Fill(dt);
-            command2.ExecuteNonQuery();
-            //Export
-            using (ExcelEngine engine = new ExcelEngine())
-            {
-                IApplication application = engine.Excel;
-                application.DefaultVersion = ExcelVersion.Xlsx;
-                // Create a new workbook
-                IWorkbook workbook = application.Workbooks.Create(1);
-                IWorksheet Worksheet = workbook.Worksheets[0];
-                // Import data from the data table
-                Worksheet.ImportDataTable(dt, true, 1, 1, true);
-                // Create Excel table or listobject and apply table style
-                IListObject table = Worksheet.ListObjects.Create("Payroll_Reports", Worksheet.UsedRange);
-
-                table.BuiltInTableStyle = TableBuiltInStyles.TableStyleLight11;
-                // Autofit the columns
-                Worksheet.UsedRange.AutofitColumns();
-                // Save the file
-
-                Stream excelStream = File.Create(Path.GetFullPath(@" DataTable - to - Excel" + i.ToString() + ".xlsx "));
-                i++;
-
-                workbook.SaveAs(excelStream);
-                excelStream.Dispose();
-            }
-        }
-        public void tagadelete()
-        {
-            SqlConnection connection = new SqlConnection(login.connectionString);
-            connection.Open();
-            SqlCommand command = new SqlCommand("delete from Payroll", connection);
-            command.ExecuteNonQuery();
-        }
-        public void tagaInsertPayroll()
-        {
-            SqlConnection connection = new SqlConnection(login.connectionString);
-            connection.Open();
-            string ExportAndArchive = " insert into Payroll (EmployeeID,BasicRate,TotalRecordHours,TotalRegularhours,TotalOvertimeHours,GrossSalary) " +
-                   "select A.EmployeeID, A.BasicRate, A.TotalHours, A.TotalHours-A.OverTimeHours, A.OverTimeHours, A.GrossSalary from Computation A group by A.EmployeeID , A.BasicRate, A.TotalHours, A.OverTimeHours, A.GrossSalary " +
-                   " insert into PayrollArchive select * from Payroll ";
-            SqlCommand command = new SqlCommand(ExportAndArchive, connection);
-            command.ExecuteNonQuery();
-        }
-
-        private void dgvDailyPayrollReport_CellClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (dgvDailyPayrollReport.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
-            {
-                dgvDailyPayrollReport.CurrentRow.Selected = true;
-            }
         }
     }
 }
